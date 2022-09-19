acfc75768d70940b7ee6792d93f4e369c5ba920d
Saikrishna <113940945+saivsk@users.noreply.github.com>
2022-09-20 02:10:11 +0530

    Update intro.java

--- intro.java
+++ intro.java
@@ -1,7 +1,7 @@
 hello! welocme to git hub
 
 
-updated the file
+Git is a distributed version control system, which means that a local clone of the project is a complete version control repository. These fully functional local repositories make it easy to work offline or remotely
 
 
 
